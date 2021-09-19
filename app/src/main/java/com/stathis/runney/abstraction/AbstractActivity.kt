4e08641d0ec514d9ac7f56b831b8fd6cfb891476
stathiskaradimitriou <stathis.karadimitriou@gmail.com>
2021-09-20 00:09:39 +0300

    Abstraction & Basic Libraries

--- app/src/main/java/com/stathis/runney/abstraction/AbstractActivity.kt
+++ app/src/main/java/com/stathis/runney/abstraction/AbstractActivity.kt
@@ -1,4 +1,26 @@
 package com.stathis.runney.abstraction
 
-abstract class AbstractActivity {
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+abstract class AbstractActivity : AppCompatActivity() {
+
+    abstract fun init()
+    abstract fun startOps()
+    abstract fun stopOps()
+
+    override fun onPostCreate(savedInstanceState: Bundle?) {
+        super.onPostCreate(savedInstanceState)
+        init()
+    }
+
+    override fun onPostResume() {
+        super.onPostResume()
+        startOps()
+    }
+
+    override fun onStop() {
+        stopOps()
+        super.onStop()
+    }
 }
--- app/src/main/java/com/stathis/runney/abstraction/AbstractFragment.kt
+++ app/src/main/java/com/stathis/runney/abstraction/AbstractFragment.kt
@@ -1,4 +1,27 @@
 package com.stathis.runney.abstraction
 
-abstract class AbstractFragment {
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.Fragment
+
+abstract class AbstractFragment : Fragment() {
+
+    abstract fun init()
+    abstract fun startOps()
+    abstract fun stopOps()
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        init()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        startOps()
+    }
+
+    override fun onStop() {
+        stopOps()
+        super.onStop()
+    }
 }
