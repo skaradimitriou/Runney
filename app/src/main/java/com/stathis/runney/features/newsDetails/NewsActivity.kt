c7c8150437cd7e8960301d1979f957764899cc72
stathiskaradimitriou <stathis.karadimitriou@gmail.com>
2021-10-09 23:23:05 +0300

    News Details Screen | Add/ Remove from favorites

--- app/src/main/java/com/stathis/runney/features/newsDetails/NewsActivity.kt
+++ app/src/main/java/com/stathis/runney/features/newsDetails/NewsActivity.kt
@@ -4,16 +4,19 @@ import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
 import androidx.lifecycle.ViewModelProvider
-import com.google.gson.Gson
 import com.stathis.runney.R
 import com.stathis.runney.abstraction.AbstractActivity
 import com.stathis.runney.databinding.ActivityNewsBinding
 import com.stathis.runney.models.News
+import android.content.Intent
+import androidx.lifecycle.Observer
 
 class NewsActivity : AbstractActivity() {
 
     private lateinit var viewModel : NewsViewModel
     private lateinit var binding : ActivityNewsBinding
+    private lateinit var news : News
+    private var favorite = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -26,25 +29,59 @@ class NewsActivity : AbstractActivity() {
     }
 
     override fun startOps() {
-        val model = intent.getSerializableExtra("NEWS") as News
-        model?.let{
+        news = intent.getSerializableExtra("NEWS") as News
+        news?.let{
             binding.news = it
+
+            viewModel.checkIfFavorite(it)
+
             supportActionBar?.title = it.title
             supportActionBar?.setDisplayHomeAsUpEnabled(true)
         }
+
+        viewModel.isFavorite.observe(this,Observer{
+            favorite = when(it){
+                true -> {
+                    true
+                }
+                false -> false
+            }
+        })
+
+
     }
 
     override fun stopOps() {
-        //
+        viewModel.isFavorite.removeObservers(this)
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
-        menuInflater.inflate(R.menu.menu,menu)
+        menuInflater.inflate(R.menu.details_menu,menu)
         return true
     }
-
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        return super.onOptionsItemSelected(item)
-        //handle the other items when clicked
+        return when(item.itemId){
+            R.id.share_btn -> {
+                shareNews()
+                true
+            }
+
+            R.id.favorite_btn -> {
+                when(favorite){
+                    true -> viewModel.removeFromFavorites(news)
+                    false -> viewModel.addToFavorites(news)
+                }
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+
+    private fun shareNews() {
+        val sharingIntent = Intent(Intent.ACTION_SEND).also{
+            it.type = "text/plain"
+            it.putExtra(Intent.EXTRA_TEXT, "You can learn more about ${news.title} by clicking here.")
+        }
+        startActivity(Intent.createChooser(sharingIntent, "Share using"))
     }
 }
--- app/src/main/java/com/stathis/runney/features/newsDetails/NewsViewModel.kt
+++ app/src/main/java/com/stathis/runney/features/newsDetails/NewsViewModel.kt
@@ -1,6 +1,66 @@
 package com.stathis.runney.features.newsDetails
 
+import android.app.Application
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.gson.Gson
+import com.stathis.runney.abstraction.AbstractViewModel
+import com.stathis.runney.models.News
 
-class NewsViewModel : ViewModel() {
+class NewsViewModel(app : Application) : AbstractViewModel(app) {
+
+    private val firestore by lazy { FirebaseFirestore.getInstance() }
+    private val auth by lazy { FirebaseAuth.getInstance() }
+    val isFavorite = MutableLiveData<Boolean>()
+    var favoriteList = mutableListOf<News>()
+
+    fun checkIfFavorite(news: News) {
+        firestore.collection("favorites")
+            .document(auth.currentUser!!.uid).get().addOnCompleteListener { task ->
+                when (task.isSuccessful) {
+                    true -> {
+                        val result = task.result
+
+                        result?.data?.toList()?.forEach {
+                            val json = Gson().toJsonTree(it.second)
+                            favoriteList = Gson().fromJson(json, Array<News>::class.java).toMutableList()
+                        }
+
+                        when (favoriteList.contains(news)) {
+                            true -> isFavorite.value = true
+                            false -> isFavorite.value = false
+                        }
+                    }
+                }
+            }
+    }
+
+    fun addToFavorites(news: News) {
+        when (favoriteList.contains(news)) {
+            true -> Unit // doc exists in favorites
+            false -> {
+                favoriteList.add(0, news)
+
+                val docRef = firestore.collection("favorites").document(auth.currentUser!!.uid)
+                val data = hashMapOf("favoriteList" to favoriteList)
+
+                docRef.set(data).addOnSuccessListener {
+                    checkIfFavorite(news)
+                }
+            }
+        }
+    }
+
+    fun removeFromFavorites(news: News) {
+        favoriteList.remove(news)
+
+        val docRef = firestore.collection("favorites").document(auth.currentUser!!.uid)
+        val data = hashMapOf("favoriteList" to favoriteList)
+
+        docRef.set(data).addOnSuccessListener {
+            checkIfFavorite(news)
+        }
+    }
 }
